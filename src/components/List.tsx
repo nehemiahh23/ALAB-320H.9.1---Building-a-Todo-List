e37b55dd3a60cda9dfd6087cde25446aa8bea34e
nehemiahh23 <nehemiahelihenry@gmail.com>
2026-02-20 18:50:09 -0500

    create component skeleton

---
+++ src/components/List.tsx
@@ -0,0 +1,12 @@
+import Input from "./Input.js"
+import Item from "./Item.js"
+
+function List() {
+  return (
+	<div>
+		<Input />
+		<Item /> {/* map from state */}		
+	</div>
+  )
+}
+export default List
